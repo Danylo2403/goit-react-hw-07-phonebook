9304b5d9e449e8886eceae72556a71a9969742ee
Danylo2403 <daniilkon24@gmail.com>
2023-12-14 21:31:03 +0200

    Initial commit

---
+++ src/components/App.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const AppLayout = styled.div`
+  max-width: 1200px;
+  min-height: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  outline: 2px solid #3498db;
+  background-color: #ecf0f1;
+  border-radius: 10px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+`;
